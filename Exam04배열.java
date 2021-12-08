7a76838efd804a4bdf78a1829691d249138903d2
beoumsu kang <95010758+Kangsaga@users.noreply.github.com>
2021-12-08 09:08:07 +0900

    Add files via upload

---
+++ Exam04배열.java
@@ -0,0 +1,24 @@
+
+public class Exam04배열 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//길이 5인 정수형 배열 array 생성
+		int [] array = new int[5];
+		
+		array[0] = 27;
+		array[1] = 30;
+		array[2] = 15;
+		array[3] = 44;
+		array[4] = 31;
+		
+		
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		System.out.println(array[2]);
+		System.out.println(array[3]);
+		System.out.println(array[4]);
+		
+	}
+
+}
